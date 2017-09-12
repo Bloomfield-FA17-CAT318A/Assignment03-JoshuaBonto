8648915dc6bc10822ae5469d8043996e577c3180
jbonto <jrbonto1002@gmail.com>
2017-09-12 17:05:25 -0400

    Add files via upload

---
+++ Assignment03-JoshuaBonto/Assets/Scripts/dataController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class dataController : MonoBehaviour {
+	public float varA=0f, varB=0f, varC=0f;
+	public Text vtA, vtB, vtC;
+	// Use this for initialization
+	void Start () {
+		//loads the data and updates the UI
+		varA = PlayerPrefs.GetFloat ("sVarA");
+		varB = PlayerPrefs.GetFloat ("sVarB");
+		varC = PlayerPrefs.GetFloat ("sVarC");
+
+
+		vtA.text = "Variable A: " + varA.ToString ();
+		vtB.text = "Variable B: " + varB.ToString ();
+		vtC.text = "Variable C: " + varC.ToString ();
+	}
+
+	public void modifyData(char c){
+		//based on the char passed the script increases a variable, and updates the UI
+		if (c == 'a') {
+			varA++;
+			vtA.text = "Variable A: " + varA.ToString ();
+		} else if (c == 'b') {
+			varB++;
+			vtB.text = "Variable B: " + varB.ToString ();
+		} else if (c == 'c') {
+			varC++;
+			vtC.text = "Variable C: " + varC.ToString ();
+		} else {
+			Debug.Log ("Error");
+		}
+	}
+	public void resetData(){
+		varA = 0f;//set the data to 0 in scene
+		varB = 0f;
+		varC = 0f;
+
+
+		vtA.text = "Variable A: " + varA.ToString ();//adjust the UI accordingly
+		vtB.text = "Variable B: " + varB.ToString ();
+		vtC.text = "Variable C: " + varC.ToString ();
+
+
+		PlayerPrefs.SetFloat ("sVarA", 0f);//set the player prefs accordingly
+		PlayerPrefs.SetFloat ("sVarB", 0f);
+		PlayerPrefs.SetFloat ("sVarC", 0f);
+		Debug.Log ("Data has been reset");
+	}
+	void OnApplicationQuit(){
+		//saves all data when you quit
+		PlayerPrefs.SetFloat ("sVarA", varA);
+		PlayerPrefs.SetFloat ("sVarB", varB);
+		PlayerPrefs.SetFloat ("sVarC", varC);
+		Debug.Log ("Application terminated, data has been saved");
+	}
+}
---
+++ Assignment03-JoshuaBonto/Assets/Scripts/dataModifier.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class dataModifier : MonoBehaviour {
+	public enum dataList{varA, varB, varC}
+	public dataList whichData;
+	public dataController thisData;
+	// The finite state machine
+
+	public void buttonPunch(){
+		if (whichData == dataList.varA) {
+			thisData.modifyData ('a');
+		} else if (whichData == dataList.varB) {
+			thisData.modifyData ('b');
+		} else {
+			thisData.modifyData ('c');
+		}
+	}
+}
